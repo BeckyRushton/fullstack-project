91dddbca9cb4e3733b9a8d8f4e4fe2239ba2a2cb
Becky Rushton <beckyrushton30@gmail.com>
2023-03-29 10:19:56 +0100

    added bookcase to homepage ready for carousel component

--- frontend/frontend-app/src/containers/HomepageContainer/HomepageContainer.jsx
+++ frontend/frontend-app/src/containers/HomepageContainer/HomepageContainer.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import BookCard from "../../components/BookCard/BookCard";
 import "./HomepageContainer.scss";
+import ShelfOfBooks from "../../assets/Images/books-shelf.png";
 
 const HomepageContainer = ({ bookData }) => {
   const populateBooks = bookData.map((book, index) => {
     return (
-      <div key={index}>
-        <BookCard book={book} />
+      <div className="book-shelves">
+        <div className="book-shelves__top">
+          <img
+            className="book-shelves__top--image"
+            src={ShelfOfBooks}
+            alt="shelfOfBooks"
+          />
+        </div>
+
+        <div className="book-shelves__middle">
+          <div key={index}>
+            <BookCard book={book} />
+          </div>
+        </div>
+
+        <div className="book-shelves__bottom">
+          <img
+            src={ShelfOfBooks}
+            className="book-shelves__bottom--image"
+            alt="shelfOfBooks"
+          />
+        </div>
       </div>
     );
   });
